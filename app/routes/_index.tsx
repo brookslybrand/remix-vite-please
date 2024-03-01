f243fb2145dc38d6483a85b90c534ab9d9b63eba
Brooks Lybrand <brookslybrand@gmail.com>
2024-03-01 15:10:59 -0600

    Defer it

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
+import { Await, defer, useLoaderData } from "@remix-run/react";
 import type { MetaFunction } from "@vercel/remix";
+import { Suspense } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +9,24 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export async function loader() {
+  return defer({
+    message1: "That was easy",
+    message2: sleep(1000).then(() => "That was streamy"),
+  });
+}
+
 export default function Index() {
+  const { message1, message2 } = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Remix + Vite + Vercel</h1>
+      <p>{message1}</p>
+      <Suspense fallback="Loading...">
+        <Await resolve={message2}>{(message2) => <p>{message2}</p>}</Await>
+      </Suspense>
     </div>
   );
 }
